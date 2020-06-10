183b4d66a69190f33d6a4e0ee18f02ba439af3b7
Kushanrawindu <kushanrawindu@gmail.com>
2020-06-10 14:13:28 +0530

    create navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movie App</title>
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
-<body>
-    
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto flex items-center justify-between px-4 py-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="#">MOVIE APP</a>
+                </li>
+                <li class="ml-16">
+                    <a href="#" class="hover:text-gray-300">Movies</a>
+                </li>
+                <li class="ml-6">
+                    <a href="#" class="hover:text-gray-300">TV Shows</a>
+                </li>
+                <li class="ml-6">
+                    <a href="#" class="hover:text-gray-300">Actors</a>
+                </li>
+            </ul>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search Movie Here..">
+                    <div class="absolute text-gray-500 top-0 pl-2 py-1">
+                        <i class="fas fa-search"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
 </body>
 </html>
